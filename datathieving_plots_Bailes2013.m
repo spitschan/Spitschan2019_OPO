487f638f31637350072ee392cd14634b8151776d
Manuel Spitschan <manuel.spitschan@psy.ox.ac.uk>
2019-02-11 14:06:59 +0100

    Vektorisierung

--- datathieving_plots_Bailes2013.m
+++ datathieving_plots_Bailes2013.m
@@ -1,31 +1,17 @@
 %Data import BAILES 2013
-HM = dlmread('human Opn4 Bailes 2013.csv'); %loading data for human melatonin sensitivity 
-EBup = dlmread('error bars upper Bailes 2013.csv'); %loading data for upper error bars human melatonin sensitivity 
-EBlow = dlmread('error bars lower Bailes 2013.csv'); %loading data for upper error bars human melatonin sensitivity 
+HM = dlmread('human Opn4 Bailes 2013.csv'); %loading data for human melatonin sensitivity
+EBup = dlmread('error bars upper Bailes 2013.csv'); %loading data for upper error bars human melatonin sensitivity
+EBlow = dlmread('error bars lower Bailes 2013.csv'); %loading data for upper error bars human melatonin sensitivity
 
- EBdif=[]; %New variable for Difference. Initializing it as empty.
-for i = 1:length(HM); %Start i loop
- EBdif(i,1)= HM(i,2)- EBup(i,2);
- EBdif(i,2)= HM(i,2)- EBlow(i,2);
-end
-
-HMtot=horzcat(HM,EBdif);
+% Save the loop
+EBdif(:, 1) = HM(:, 2)-EBup(:, 2);
+EBdif(:, 2) = HM(:, 2)-EBlow(:, 2);
 
 figure %Open new figure
-scatter(HMtot(:,1),HMtot(:,2),'or', 'filled'); %Plot the data as a scatter plot
+scatter(HM(:,1),HM(:,2),'or', 'filled'); %Plot the data as a scatter plot
 set(gca,'xlim',[400,650],'YScale', 'log', 'ylim',[0.001,2]);% set the x-axis limits and scale y to log
 xlabel('wavelength in nm'); %Label for the x-axis
 ylabel('relative sensitivity'); %Label for the y-axis
 title('human melanopsin sensitivity'); %Add a title
 hold on;
-errorbar(HMtot(:,1), HMtot(:,2),HMtot(:,3),HMtot(:,4));
-
-
-
-
-
-
-
-
-
-
+errorbar(HM(:,1), HM(:,2), EBdif(:,1), EBdif(:,2));
