501c6214c57c7d583e4f4ff2529c1a7cfab79e74
Rafael Lazar <rafael.lazar@web.de>
2019-03-14 23:31:06 +0100

    created table1 for filters, some new filter data thieved & web-based sources stored in txt file

--- Filter_melanopsin&luminance_impact.m
+++ Filter_melanopsin&luminance_impact.m
@@ -6,7 +6,7 @@ functions of luminance and melanopsin via Manuel Spitschan on gitHub
 https://github.com/spitschan
 
 date created: 06.03.2019
-date of last modification: 08.03.2019
+date of last modification: 14.03.2019
 specification: compute impact of different filter spectacles on melanopsin
 and luminance
 %}
@@ -79,15 +79,15 @@ end
 
 
  %plotten aller Filter luminance impact vs. melanopsin impact
-plot (alle_melanopsin_impact(filter_Taxonomie1==1) ,alle_luminance_throughfilter(filter_Taxonomie1==1) ,'or');
+plot (alle_melanopsin_impact(filter_taxonomy1==1) ,alle_luminance_throughfilter(filter_taxonomy1==1) ,'or');
 title('filter luminance and melanopsin impact') %Add a title
 axis('equal');
 set(gca,'xlim',[0,1],'XScale', 'log', 'ylim',[0,1], 'YScale', 'log');% set the x-axis values
 xlabel('relative impact of EEW through filter on melanopsin vs no filter'); %Label for the x-axis
 ylabel('luminance of EEW through filter in relation to no filter'); %Label for the y-axis
 hold on; 
-plot (alle_melanopsin_impact((filter_Taxonomie1==2)) ,alle_luminance_throughfilter(filter_Taxonomie1==2) ,'xk');
-plot (alle_melanopsin_impact((filter_Taxonomie1==3)) ,alle_luminance_throughfilter(filter_Taxonomie1==3) ,'+b');
-plot (alle_melanopsin_impact((filter_Taxonomie1==4)) ,alle_luminance_throughfilter(filter_Taxonomie1==4) ,'^m');
-plot (alle_melanopsin_impact((filter_Taxonomie1==5)) ,alle_luminance_throughfilter(filter_Taxonomie1==5) ,'dk');
-plot (alle_melanopsin_impact((filter_Taxonomie1==6)) ,alle_luminance_throughfilter(filter_Taxonomie1==6) ,'pg');
+plot (alle_melanopsin_impact((filter_taxonomy1==2)) ,alle_luminance_throughfilter(filter_taxonomy1==2) ,'xk');
+plot (alle_melanopsin_impact((filter_taxonomy1==3)) ,alle_luminance_throughfilter(filter_taxonomy1==3) ,'+b');
+plot (alle_melanopsin_impact((filter_taxonomy1==4)) ,alle_luminance_throughfilter(filter_taxonomy1==4) ,'^m');
+plot (alle_melanopsin_impact((filter_taxonomy1==5)) ,alle_luminance_throughfilter(filter_taxonomy1==5) ,'dk');
+plot (alle_melanopsin_impact((filter_taxonomy1==6)) ,alle_luminance_throughfilter(filter_taxonomy1==6) ,'pg');
